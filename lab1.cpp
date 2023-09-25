75ef02818c575d126895542b579061dcb3f88f95
myata64 <kalyupanov.serafim@gmail.com>
2023-09-26 02:10:29 +0300

    cout на русском

--- lab1.cpp
+++ lab1.cpp
@@ -6,20 +6,20 @@ setlocale(LC_ALL, "Russian");
     float No1[5]; // Sozdaem odnomerniy massiv s 5 elementami
 
     // 2.1. Проверить содержимое массива No1 (с помощью цикла for и операции вывода cout<<).
-    std::cout << "Soderzhimoe massiva No1:" << std::endl;
+    std::cout << "Содержимое массива No1 до ввода:"  << std::endl;
     for (int i = 0; i < 5; i++) {
         std::cout << No1[i] << " ";
     }
     std::cout << std::endl;
 
     // 2.2. Ввести данные в массив No1 (с помощью цикла for и операции ввода cin>>).
-    std::cout << "Vvedite 5 chisel massiva No1:" << std::endl;
+    std::cout << "Введите 5 числел массива No1:" << std::endl;
     for (int i = 0; i < 5; i++) {
         std::cin >> No1[i];
     }
 
     // 2.3. Еще раз проверить содержимое этого массива, сделать выводы.
-    std::cout << "Soderzhimoe massiva No1 posle vvoda:" << std::endl;
+    std::cout << "Содержимое массива No1 после ввода:" << std::endl;
     for (int i = 0; i < 5; i++) {
         std::cout << No1[i] << " ";
     }
@@ -27,30 +27,30 @@ setlocale(LC_ALL, "Russian");
 
     // 2.4. Присвоить указателю No2 адрес массива No1, вывести на экран адреса массива и указателя и содержимое указателя.
     float* No2 = No1;
-    std::cout << "Adress massiva No1: " << No1 << std::endl;
-    std::cout << "Adres ukazatelya No2: " << No2 << std::endl;
-    std::cout << "Soderzhimoe ukazatelya No2: " << *No2 << std::endl;
+    std::cout << "Адрес массива No1: " << No1 << std::endl;
+    std::cout << "Адрес указателя No2: " << No2 << std::endl;
+    std::cout << "Содержимое указателя No2: " << *No2 << std::endl;
 
     // 2.5. Повторить пункт 3 для указателя, содержащего адрес массива.
-    float (*No3)[5] = &No1; // ob'yavlyaet ukazatel *No3 na massiv tipa float razmerom 5 i inizializiruet ego adresom massiva No1
-    std::cout << "Adres ukazatelya No3: " << No3 << std::endl;
-    std::cout << "Soderzhimoe ukazatelya No3: " << (*No3)[0] << std::endl;
+    float (*No3)[5] = &No1; // Объявляет указатель No3 на массив типа float, размерности 5 и инициализирует его массивом адреса No1
+    std::cout << "Адрес указателя No3: " << No3 << std::endl;
+    std::cout << "Содержимое указателя No3: " << (*No3)[0] << std::endl;
 
     // 2.6. Повторить пункты 1-3 для статического массива No3.
         // 2.6 -> 2.1
     char No3_static[5]; //Sozdaem static massiv simvolov s 10 elementami
-    std::cout << "2.6 -> 2.1 Soderzhimoe do vvoda znacheniy:" << std::endl;
+    std::cout << "2.6 -> 2.1 Содержимое до ввода значений:" << std::endl;
     for (int i =0; i<5; i++) {
         std::cout << No3_static[i] << " ";
     } 
     std::cout << std::endl;
         // 2.6 -> 2.2
-    std::cout << "2.6 -> 2.2 Vvedite 5 simvolov massiva No3_static:" << std::endl; 
+    std::cout << "2.6 -> 2.2 Введите 5 символов для No3_static:" << std::endl; 
     for (int i = 0; i < 5; i++) {
     std::cin >> No3_static[i];
     }   
         // 2.6 -> 2.3
-    std::cout << "2.6 -> 2.3 Elementi simvolnogo massiva:" << std::endl;
+    std::cout << "2.6 -> 2.3 Элементы массива после ввода:" << std::endl;
     for (int i=0; i<5; i++) {
         std::cout << No3_static[i] << " ";
         } 
@@ -60,11 +60,11 @@ setlocale(LC_ALL, "Russian");
     // 2.7. Используя имеющийся указатель No2, создать динамический массив и повторить для него пункты 1 - 3.
     int size = 3;
     No2 = new float[size];
-    std::cout << "Vvedite " << size << " chisel dlya dinamic massiva:" << std::endl;
+    std::cout << "Введите " << size << " чисел для динамического массива:" << std::endl;
     for (int i = 0; i < size; i++) {
         std::cin >> No2[i];
     }
-    std::cout << "Soderzhimoe dinamic massiva:" << std::endl;
+    std::cout << "Содержимое динамического массива:" << std::endl;
     for (int i = 0; i < size; i++) {
         std::cout << No2[i] << " ";
     }
@@ -81,14 +81,14 @@ setlocale(LC_ALL, "Russian");
         No4[i] = new char[stb];
         }
         //2.8 -> 2.2
-    std::cout << "2.8 -> 2.2 Vvedite elementi char massiva" << std::endl;
+    std::cout << "2.8 -> 2.2 Введите 4 символа для массива:" << std::endl;
     for(int i = 0; i < str; i++) {
         for(int j = 0; j < stb; j++) {
                std::cin >> No4[i][j];
         }
     }
         //2.8 -> 2.3
-    std::cout << "2.8 -> 2.3 Elementi char massiva" << std::endl;
+    std::cout << "2.8 -> 2.3 Элементы массива:" << std::endl;
     for(int i = 0; i < str; i++) {
         for(int j = 0; j < stb; j++) {
                std::cout << No4[i][j] << " ";
@@ -108,12 +108,13 @@ setlocale(LC_ALL, "Russian");
     
     // 2.10. Повторить пункт 9, используя имя массива как указатель и операцию доступа по указателю.
     unsigned int (*No7)[3][4] = (unsigned int (*)[3][4])No5;
-    std::cout << "Element trehmernogo massiva No5 cherez ukazatel No6: " << No7[1][2][3] << std::endl;
+    std::cout << "Элементы трехмерного массива No5 через указатель No6: " << No7[1][2][3] << std::endl;
     
     
     // 2.11. Присвоить указателю No6 на двумерный массив адрес трехмерного массива No5. Повторить для этого указателя пункт 10.
     char (*No6)[3][4]; // No6 теперь указатель на двумерный массив символов
     No6 = (char (*)[3][4])No5; // приведение типа массива No5 к типу указателя No6
+    std::cout << " Вывод элемента:" << std::endl;
     std::cout << No6[1][2][3] << std::endl; // доступ к элементу через No6
 
     return 0;
